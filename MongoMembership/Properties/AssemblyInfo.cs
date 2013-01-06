b9863831ecb658b701575d7e827ad044480b38f4
FoC- <nickola.kush@gmail.com>
2013-01-06 17:53:49 +0200

    Update version of package

--- MongoMembership/Properties/AssemblyInfo.cs
+++ MongoMembership/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MongoMembership")]
-[assembly: AssemblyCopyright("FoC © 2012")]
+[assembly: AssemblyCopyright("FoC © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.13")]
-[assembly: AssemblyFileVersion("0.0.0.13")]
+[assembly: AssemblyVersion("0.0.0.14")]
+[assembly: AssemblyFileVersion("0.0.0.14")]
 [assembly: InternalsVisibleTo("MongoMembership.Tests")]
