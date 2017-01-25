8a8cc64d587a7275c52502fd3863cf3e052c3356
Nikolay Kush <nickola.kush@gmail.com>
2017-01-25 23:17:36 +0100

    Core: update target framework to 4.5.2 and minor update for driver

--- MongoMembership/Properties/AssemblyInfo.cs
+++ MongoMembership/Properties/AssemblyInfo.cs
@@ -2,36 +2,22 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+
 [assembly: AssemblyTitle("MongoMembership")]
 [assembly: AssemblyDescription("Provide ability to use ASP.NET providers with MongoDB storage")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MongoMembership")]
-[assembly: AssemblyCopyright("FoC © 2013")]
+[assembly: AssemblyCopyright("FoC © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45e81b54-6648-4d60-88fa-a97eb4b66e72")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
-[assembly: InternalsVisibleTo("MongoMembership.Tests")]
+[assembly: Guid("5fac9e68-b848-4b2c-b5ef-86349661c5c3")]
+
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
