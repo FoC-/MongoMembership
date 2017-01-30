3c0dcebd4750347a81348e303598db5828ba269d
Nikolay Kush <nickola.kush@gmail.com>
2017-01-30 22:10:49 +0100

    Assembly info: fixed

--- MongoMembership/Properties/AssemblyInfo.cs
+++ MongoMembership/Properties/AssemblyInfo.cs
@@ -1,23 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
 
 [assembly: AssemblyTitle("MongoMembership")]
 [assembly: AssemblyDescription("Provide ability to use ASP.NET providers with MongoDB storage")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MongoMembership")]
-[assembly: AssemblyCopyright("FoC © 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-
 [assembly: ComVisible(false)]
-
-
 [assembly: Guid("5fac9e68-b848-4b2c-b5ef-86349661c5c3")]
-
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Shared/GlobalAssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("MongoMembership")]
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © Mykola Kush 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("0.1.0.0")]
+//Change MAJOR version before release
+//Change MINOR version when you add functionality in a backwards-compatible manner
+//Change PATCH version when you make backwards-compatible bugfixes
